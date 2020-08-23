a839481d551b0b9492ae3cce9555609575d150b6
valer1435 <valp998@gmail.com>
2020-08-23 10:23:32 +0300

    delete Normalization

--- working_baseline/predict_period.py
+++ working_baseline/predict_period.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import SGDClassifier
 import joblib
 
 
-random_seed = 4545435
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -23,26 +23,6 @@ class StyleMatrix(nn.Module):
     def forward(self, inp):
         G = gram_matrix(inp)
         return G
-
-
-cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
-cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
-
-# create a module to normalize input image so we can easily put it in a
-# nn.Sequential
-class Normalization(nn.Module):
-    def __init__(self, mean, std):
-        super(Normalization, self).__init__()
-        # .view the mean and std to make them [C x 1 x 1] so that they can
-        # directly work with image Tensor of shape [B x C x H x W].
-        # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
-
-    def forward(self, img):
-        # normalize img
-        return (img - self.mean) / self.std
-    
     
 def gram_matrix(inp):
     a, b, c, d = inp.size()  # a=batch size(=1)
